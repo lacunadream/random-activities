e8099a6c5dd271635120ac1fdfdd43c9f0a637b5
clouddream <d.s.yong@lse.ac.uk>
2015-11-07 04:48:36 +0000

    random logic

--- controllers/home.js
+++ controllers/home.js
@@ -10,6 +10,20 @@ exports.index = function(req, res) {
 };
 
 /**
+ * POST /
+ * Obtain data
+ */
+
+exports.postDetails = function(req, res, next){
+	var input1 = req.body.postcode;
+	var input2 = req.body.budget;
+	var input3
+	var inptu4
+	//console.log(input);
+	res.redirect('/results?pc=' + input1 + '&bud=' + input2);
+}
+
+/**
  * GET /
  * Massive wtf
  */
@@ -24,20 +38,18 @@ exports.randomActivity = function(req, res){
 	function random (low, high) {
 		return Math.random() * (high - low) + low;
 	}
+	var x = 50
+	var y = 100
+	var z = random(x,y)
+	if (z > 75) {
+		res.render('home2');
+	} else {
+		res.render('homepage');
+	}
 
 	// Grab Time
 	var date = new Date();
 	var current_hour = date.getHours();
 	console.log(current_hour)
-
-	res.render('home2');
 }
 
-exports.postDetails = function(req, res, next){
-	var input1 = req.body.postcode;
-	var input2 = req.body.budget;
-	var input3
-	var inptu4
-	//console.log(input);
-	res.redirect('/results?pc=' + input1 + '&bud=' + input2);
-}
