80dfecb65c05c17c75d923e4203a526d0e1cfad4
clouddream <d.s.yong@lse.ac.uk>
2015-11-07 11:18:12 +0000

    exploring scope

--- controllers/home.js
+++ controllers/home.js
@@ -27,29 +27,33 @@ exports.postDetails = function(req, res, next){
  * GET /
  * Massive wtf
  */
+	// Ahh scoping
+	function random (low, high) {
+		return Math.random() * (high - low) + low;
+	}
 
 exports.randomActivity = function(req, res){
 	// Process Queries
 	var input1 = req.query.pc;
 	var input2 = req.query.bud;
-	console.log('omg is ' + input1 + ' ' + input2);
+	// console.log('omg is ' + input1 + ' ' + input2);
 
-	//Random Generator
-	function random (low, high) {
-		return Math.random() * (high - low) + low;
-	}
-	var x = 50
-	var y = 100
-	var z = random(x,y)
-	if (z > 75) {
-		res.render('home2');
-	} else {
-		res.render('homepage');
-	}
+	// Random Generator
+
 
 	// Grab Time
 	var date = new Date();
 	var current_hour = date.getHours();
 	console.log(current_hour)
+
+	var x = 50
+	var y = 100
+	var z = random(x,y)
+	console.log(z)
+	if (z > 75) {
+		res.render('home2');
+	} else {
+		res.render('homepage');
+	}
 }
 
